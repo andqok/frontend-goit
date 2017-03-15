b27de1af82f1531d1438593c3145e8765eb6145d
Andrew Domanski <andqok@gmail.com>
2017-03-15 20:46:13 +0200

    improved code style hw 13

---
+++ Homework js_15-16 (Searchbox, Prototypal Inheritance)/gulpfile.js
@@ -0,0 +1,56 @@
+const gulp        = require('gulp');
+const haml        = require('gulp-ruby-haml');
+const sass        = require('gulp-sass');
+const browserSync = require('browser-sync');
+const babili      = require('gulp-babili');
+const htmlmin     = require('gulp-htmlmin');
+const cleanCSS    = require('gulp-clean-css');
+const reload      = browserSync.reload;
+const paths       = {
+  html:['index.html'],
+  css:['main.scss']
+}
+
+gulp.task('haml', function () {
+  gulp.src('src/index.haml')
+  .pipe(haml())
+  .pipe(htmlmin({collapseWhitespace: true}))
+  .pipe(gulp.dest('dist'))
+  .pipe(reload({stream:true}));
+});
+
+gulp.task('sass', function() {
+  gulp.src('src/sass/style.sass')
+  .pipe(sass()
+  .on('error', sass.logError))
+  .pipe(cleanCSS())
+  .pipe(gulp.dest('dist/css'))
+  .pipe(reload({stream:true}));
+});
+
+gulp.task('minify', function () {
+  gulp.src('src/js/*.js')
+    .pipe(babili({
+      mangle: {
+        keepClassNames: true
+      }
+    }))
+  .pipe(gulp.dest('dist/js'));
+
+});
+
+gulp.task('browserSync', function() {
+  browserSync({
+    server: {
+      baseDir: "./dist"
+    },
+    port: 8080,
+    open: true,
+    notify: false
+  });
+});
+
+gulp.task('default', ['haml', 'sass', 'minify', 'browserSync']);
+gulp.watch('src/index.haml', ['haml']);
+gulp.watch('src/sass/style.sass', ['sass']);
+gulp.watch('src/js/*.js', ['minify']);
